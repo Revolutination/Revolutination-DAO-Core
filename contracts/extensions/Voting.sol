87c82641e97ca30f160b8eb55124c1ee339a649a
KOSASIH <kosasihg88@gmail.com>
2024-03-22 02:30:29 +0700

    Create Voting.sol

---
+++ contracts/extensions/Voting.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Voting is Ownable {
+    struct Candidate {
+        uint256 id;
+        string name;
+        uint256 voteCount;
+    }
+
+    mapping(uint256 => Candidate) public candidates;
+    mapping(address => bool) public voters;
+
+    uint256 public candidatesCount;
+
+    event votedEvent(uint256 indexed _candidateId);
+
+    constructor() {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string memory _name) private onlyOwner {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint256 _candidateId) public {
+        require(!voters[msg.sender], "Voter has already voted.");
+        require(_candidateId > 0 && _candidateId <= candidatesCount, "Invalid candidate.");
+
+        voters[msg.sender] = true;
+        candidates[_candidateId].voteCount++;
+
+        emit votedEvent(_candidateId);
+    }
+}
